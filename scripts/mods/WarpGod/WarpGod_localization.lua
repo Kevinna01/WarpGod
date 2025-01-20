846fd6e502c4218485e1344a73be88034d0098d9
Kevinna01 <97471980+Kevinna01@users.noreply.github.com>
2025-01-20 18:41:52 +0100

    Update WarpGod_localization.lua

--- scripts/mods/WarpGod/WarpGod_localization.lua
+++ scripts/mods/WarpGod/WarpGod_localization.lua
@@ -17,17 +17,11 @@ local loc = {
     prevent_psyker_explosion_enable_description = {
         en = "Prevents peril-generating attacks when peril exceeds the threshold whilst Warp Unbound ability is not active.",
     },
-    macro_anti_detection_enable = {
-        en = "Enable Macro Anti-Detection",
-    },
-    macro_anti_detection_enable_description = {
-        en = "Prevents quelling when exceeding threshold, used to hide usage of quell-cancel macro. Default is Off.",
-    },
     peril_threshold = {
         en = "Peril Threshold",
     },
     peril_threshold_description = {
-        en = "Maximum allowable peril before peril-generating attacks are disabled. Default is 1.0.",
+        en = "Maximum allowable peril before peril-generating attacks are disabled.",
     },
     -- Warp Unbound Bug Fix
     warp_unbound_bug_fix = {
@@ -37,56 +31,20 @@ local loc = {
         en = "Enable Warp Unbound Bug Fix",
     },
     warp_unbound_bug_fix_enable_description = {
-        en = "Warp Unbound last 11.5s, at sometime around 10.5s there is a probability to explode with Smite, Surge & Trauma staffs. Bugfix disables attacks that generate peril during a interval.",
+        en = "There is a probability to explode with Smite, Surge & Trauma staffs despite activating Warp Unbound. Bugfix disables attacks that generate peril when entering and exiting the infinite-casting state.",
     },
-    interval1_duration = {
-        en = "Start-interval duration",
+    debounce_enter_percentage = {
+        en = "Debounce Enter Percentage",
     },
-    interval1_duration_description = {
-        en = "For the bugfix interval. Default is 0.7",
+    debounce_enter_percentage_description = {
+        en = "At what peril percentage are perilous inputs disabled until warp unbound buff becomes activated.",
     },
-    interval1_start_delay = {
-        en = "Start-interval offset",
+    debounce_exit_time = {
+        en = "Debounce Exit Time",
     },
-    interval1_start_delay_description = {
-        en = "For the Bugfix interval interval to offset interval to start later. Default is 0.9",
+    debounce_exit_time_description = {
+        en = "At how many seconds left of the Warp Unbound buff timer are perilous inputs disabled until buff becomes disabled.",
     },
-    interval2_duration = {
-        en = "End-interval duration",
-    },
-    interval2_duration_description = {
-        en = "For the end of Warp Unbound buff. Default is 0.5",
-    },
-    interval2_end_delay = {
-        en = "End-interval offset",
-    },
-    interval2_end_delay_description = {
-        en = "For the end of Warp Unbound buff to offset interval to end later. Default is 0.2",
-    },
-    --[[
-    --Additional Settings
-    additional_settings = {
-        en = "Additional Settings",
-    },
-    auto_quell = {
-        en = "Auto Quell",
-    },
-    auto_quell_description = {
-        en = "Auto Quell when exceeding threshold",
-    },
-    auto_quell_threshold = {
-        en = "Auto Quell Threshold",
-    },
-    auto_quell_threshold_description = {
-        en = "Auto Quell until below auto quell-threshold. Should be less than peril-threshold",
-    },
-    auto_warp_unbound = {
-        en = "Auto Warp Unbound",
-    },
-    auto_warp_unbound_description = {
-        en = "Automatically activate Warp Unbound when timer ends, if at 100%% peril",
-    },
-    --]]
 }
 
 return loc

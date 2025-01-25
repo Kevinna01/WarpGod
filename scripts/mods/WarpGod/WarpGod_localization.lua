0251fb6f48c4e8acbb7579c456937efd348c78e6
Kevinna01 <97471980+Kevinna01@users.noreply.github.com>
2025-01-26 00:05:29 +0100

    Update WarpGod_localization.lua

--- scripts/mods/WarpGod/WarpGod_localization.lua
+++ scripts/mods/WarpGod/WarpGod_localization.lua
@@ -5,7 +5,7 @@ local loc = {
         en = "Warp God",
     },
     mod_description = {
-        en = "Prevent Peril of the Warp Explosion & Warp Unbound talent bug hotfix",
+        en = "Prevent Peril of the Warp Explosion & Warp Unbound talent bug hotfix & Auto Quelling & Auto Ability Activation",
     },
     -- Prevent Psyker Explosion
     prevent_psyker_explosion = {
@@ -18,10 +18,10 @@ local loc = {
         en = "Prevents peril-generating attacks when peril exceeds the threshold whilst Warp Unbound ability is not active.",
     },
     peril_threshold = {
-        en = "Peril Threshold",
+        en = "Peril Threshold Percentage",
     },
     peril_threshold_description = {
-        en = "Maximum allowable peril before peril-generating attacks are disabled.",
+        en = "Maximum allowable peril before peril-generating attacks are disabled. [Default 0.95]",
     },
     -- Warp Unbound Bug Fix
     warp_unbound_bug_fix = {
@@ -37,13 +37,48 @@ local loc = {
         en = "Debounce Enter Percentage",
     },
     debounce_enter_percentage_description = {
-        en = "At what peril percentage are perilous inputs disabled until warp unbound buff becomes activated.",
+        en = "At what peril percentage are perilous inputs disabled until warp unbound buff becomes activated. [Default 0.96]",
     },
     debounce_exit_time = {
-        en = "Debounce Exit Time",
+        en = "Debounce Exit Time.",
     },
     debounce_exit_time_description = {
-        en = "At how many seconds left of the Warp Unbound buff timer are perilous inputs disabled until buff becomes disabled.",
+        en = "At how many seconds left of the Warp Unbound buff timer are perilous inputs disabled until buff becomes disabled. [Default 0.5]",
+    },
+    -- Auto Quell
+    auto_quell = {
+        en = "Auto Quell",
+    },
+    auto_quell_enable = {
+        en = "Enable Auto Quell",
+    },
+    auto_quell_threshold = {
+        en = "Auto Quell Threshold",
+    },
+    auto_quell_threshold_description = {
+        en = "[Default 0.95]",
+    },
+    auto_quell_duration = {
+        en = "Auto Quell Duration.",
+    },
+    auto_quell_duration_description = {
+        en = "[Default 0.5]",
+    },
+    -- Auto Ability Activation
+    auto_ability = {
+        en = "Auto Ability",
+    },
+    auto_gaze_enable = {
+        en = "Auto Scriers Gaze",
+    },
+    auto_gaze_enable_description = {
+        en = "Activate Scrierz Gaze automatically (if warp unbound is equipped) when the timer reaches the end.",
+    },
+    auto_vent_enable = {
+        en = "Auto Venting Shriek",
+    },
+    auto_vent_enable_description = {
+        en = "Activate Venting Shriek automatically if current peril is above peril threshold.",
     },
 }
 
